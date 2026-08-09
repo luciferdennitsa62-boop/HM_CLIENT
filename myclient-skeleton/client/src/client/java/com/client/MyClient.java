194579021c7b5fc4dfa896c5b7bdb2fe1da4eb5f
luciferdennitsa62-boop <luciferdennitsa62@gmail.com>
2026-08-09 17:35:47 +0300

    Rewrite client entrypoint for Fabric 1.21.11

--- myclient-skeleton/client/src/client/java/com/client/MyClient.java
+++ myclient-skeleton/client/src/client/java/com/client/MyClient.java
@@ -1,65 +1,59 @@
 package com.client;
 
 import com.client.event.EventBus;
+import com.client.impl.gui.screen.ClickGuiScreen;
 import com.client.impl.module.ModuleManager;
-import com.mojang.blaze3d.platform.InputConstants;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 import org.lwjgl.glfw.GLFW;
 
-/**
- * Точка входа клиента.
- */
-public class MyClient implements ClientModInitializer {
+public final class MyClient implements ClientModInitializer {
+    public static final String MOD_ID = "myclient";
+    public static final String CLIENT_NAME = "HM Client";
+    public static final String CLIENT_VERSION = "0.1.0";
 
-	public static final String MOD_ID = "myclient";
-	public static final String CLIENT_NAME = "MyClient";
-	public static final String CLIENT_VERSION = "0.1.0";
+    private static MyClient instance;
+    private EventBus eventBus;
+    private ModuleManager moduleManager;
+    private KeyMapping openGuiKeyBinding;
 
-	private static MyClient instance;
-	private EventBus eventBus;
-	private ModuleManager moduleManager;
-	private KeyMapping openGuiKeyBinding;
+    @Override
+    public void onInitializeClient() {
+        instance = this;
+        eventBus = new EventBus();
+        moduleManager = new ModuleManager();
+        moduleManager.registerModules();
 
-	@Override
-	public void onInitializeClient() {
-		instance = this;
+        openGuiKeyBinding = KeyBindingHelper.registerKeyBinding(new KeyMapping(
+                "key." + MOD_ID + ".open_gui",
+                GLFW.GLFW_KEY_RIGHT_SHIFT,
+                KeyMapping.Category.MISC
+        ));
 
-		this.eventBus = new EventBus();
-		this.moduleManager = new ModuleManager();
-		this.moduleManager.registerModules();
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (openGuiKeyBinding.consumeClick()) {
+                if (client.screen == null) {
+                    client.setScreen(new ClickGuiScreen());
+                }
+            }
+            eventBus.postTick();
+        });
 
-		this.openGuiKeyBinding = KeyBindingHelper.registerKeyBinding(new KeyMapping(
-				"key." + MOD_ID + ".open_gui",
-				InputConstants.Type.KEYSYM,
-				GLFW.GLFW_KEY_RIGHT_SHIFT,
-				KeyMapping.Category.MISC
-		));
+        System.out.println("[" + CLIENT_NAME + "] initialized, Minecraft 1.21.11, Fabric");
+    }
 
-		ClientTickEvents.END_CLIENT_TICK.register(client -> {
-			while (openGuiKeyBinding.consumeClick()) {
-				if (Minecraft.getInstance().screen == null) {
-					Minecraft.getInstance().setScreen(new com.client.impl.gui.screen.ClickGuiScreen());
-				}
-			}
-			eventBus.postTick();
-		});
+    public static MyClient getInstance() {
+        return instance;
+    }
 
-		System.out.println("[" + CLIENT_NAME + "] initialized, v" + CLIENT_VERSION);
-	}
+    public EventBus getEventBus() {
+        return eventBus;
+    }
 
-	public static MyClient getInstance() {
-		return instance;
-	}
-
-	public EventBus getEventBus() {
-		return eventBus;
-	}
-
-	public ModuleManager getModuleManager() {
-		return moduleManager;
-	}
+    public ModuleManager getModuleManager() {
+        return moduleManager;
+    }
 }
