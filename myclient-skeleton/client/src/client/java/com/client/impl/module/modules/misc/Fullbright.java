7e8f2308b3b093fa17189747d5b0118cf205d3f1
luciferdennitsa62-boop <luciferdennitsa62@gmail.com>
2026-08-09 17:36:29 +0300

    Rewrite Fullbright for Minecraft 1.21.11 options API

--- myclient-skeleton/client/src/client/java/com/client/impl/module/modules/misc/Fullbright.java
+++ myclient-skeleton/client/src/client/java/com/client/impl/module/modules/misc/Fullbright.java
@@ -3,29 +3,24 @@ package com.client.impl.module.modules.misc;
 import com.client.impl.module.Category;
 import com.client.impl.module.Module;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.OptionInstance;
 
-/**
- * Полностью клиентская фича — просто выставляет игровую опцию яркости
- * в максимум, пока модуль включён, и возвращает как было при выключении.
- */
-public class Fullbright extends Module {
+public final class Fullbright extends Module {
+    private double previousGamma = 1.0;
 
-	private double previousGamma;
+    public Fullbright() {
+        super("Fullbright", "Максимальная яркость мира", Category.RENDER);
+    }
 
-	public Fullbright() {
-		super("Fullbright", "Максимальная яркость мира", Category.RENDER);
-	}
+    @Override
+    protected void onEnable() {
+        Minecraft client = Minecraft.getInstance();
+        previousGamma = client.options.gamma().get();
+        client.options.gamma().set(16.0);
+    }
 
-	@Override
-	protected void onEnable() {
-		OptionInstance<Double> gammaOption = Minecraft.getInstance().options.gamma();
-		previousGamma = gammaOption.get();
-		gammaOption.set(16.0);
-	}
-
-	@Override
-	protected void onDisable() {
-		Minecraft.getInstance().options.gamma().set(previousGamma);
-	}
+    @Override
+    protected void onDisable() {
+        Minecraft client = Minecraft.getInstance();
+        client.options.gamma().set(previousGamma);
+    }
 }
