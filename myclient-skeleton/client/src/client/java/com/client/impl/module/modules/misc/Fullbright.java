1e58b88ac16b15b2b18d96d4cc79eb54d9f1afc7
luciferdennitsa62-boop <luciferdennitsa62@gmail.com>
2026-08-09 16:49:41 +0300

    Refactor Fullbright module to use new Minecraft API

--- myclient-skeleton/client/src/client/java/com/client/impl/module/modules/misc/Fullbright.java
+++ myclient-skeleton/client/src/client/java/com/client/impl/module/modules/misc/Fullbright.java
@@ -2,13 +2,12 @@ package com.client.impl.module.modules.misc;
 
 import com.client.impl.module.Category;
 import com.client.impl.module.Module;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.option.SimpleOption;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.OptionInstance;
 
 /**
  * Полностью клиентская фича — просто выставляет игровую опцию яркости
  * в максимум, пока модуль включён, и возвращает как было при выключении.
- * Никакой сети тут нет вообще.
  */
 public class Fullbright extends Module {
 
@@ -20,13 +19,13 @@ public class Fullbright extends Module {
 
 	@Override
 	protected void onEnable() {
-		SimpleOption<Double> gammaOption = MinecraftClient.getInstance().options.getGamma();
-		previousGamma = gammaOption.getValue();
-		gammaOption.setValue(16.0); // за пределами обычного слайдера (0-1), но клиент это принимает
+		OptionInstance<Double> gammaOption = Minecraft.getInstance().options.gamma();
+		previousGamma = gammaOption.get();
+		gammaOption.set(16.0);
 	}
 
 	@Override
 	protected void onDisable() {
-		MinecraftClient.getInstance().options.getGamma().setValue(previousGamma);
+		Minecraft.getInstance().options.gamma().set(previousGamma);
 	}
 }
